aa1e816575fb9858310b6dae81e642cc27b45b27
hotman521 <164437458+hotman521@users.noreply.github.com>
2024-05-02 20:03:54 -0400

    Update fsdfdsfdsfds.lua

--- fsdfdsfdsfds.lua
+++ fsdfdsfdsfds.lua
@@ -1024,6 +1024,46 @@ function Library:Window(options)
 				end)
 			end)
 		end
+
+		do -- Dragging
+			local gui = GUI["l2"]
+
+			local dragging
+			local dragInput
+			local dragStart
+			local startPos
+
+			local function update(input)
+				local delta = input.Position - dragStart
+				gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+			end
+
+			gui.InputBegan:Connect(function(input)
+				if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+					dragging = true
+					dragStart = input.Position
+					startPos = gui.Position
+
+					input.Changed:Connect(function()
+						if input.UserInputState == Enum.UserInputState.End then
+							dragging = false
+						end
+					end)
+				end
+			end)
+
+			gui.InputChanged:Connect(function(input)
+				if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+					dragInput = input
+				end
+			end)
+
+			uis.InputChanged:Connect(function(input)
+				if input == dragInput and dragging then
+					update(input)
+				end
+			end)
+		end
 	end
 	
 	function GUI:KeybindListVisibility(State)
