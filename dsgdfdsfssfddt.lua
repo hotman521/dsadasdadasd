831d2f8262012076ede2e27f143e0a5df20f1b31
hotman521 <164437458+hotman521@users.noreply.github.com>
2024-05-28 15:01:13 -0400

    Update and rename dsgdsfddt.lua to dsgdfdsfssfddt.lua

--- dsgdfdsfssfddt.lua
+++ dsgdfdsfssfddt.lua
@@ -342,7 +342,7 @@ do -- Folders
 end
 --
 if Client and ClientCharacter then
-	if trimmed_stamp ~= "5/28/2024" then
+	if trimmed_stamp ~= "5/27/2024" then
 		Client:Kick("Da Hood has updated please ping me to update the script.")
 	end
 end
